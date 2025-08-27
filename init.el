51960274fa02a6c874712f9a2c1b78adad4b6cb0
neochin <neochin@163.com>
2025-08-27 20:28:53 +0800

    add go-mode, and set whitespace-mode face color

--- init.el
+++ init.el
@@ -20,6 +20,7 @@
 (require 'init-c)
 (require 'init-python)
 (require 'init-web)
+(require 'init-go)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -28,29 +29,17 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
-     "01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
-     default))
+   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" "01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd" default))
  '(display-line-numbers-type 'relative)
  '(package-selected-packages
-   '(auctex-latexmk benchmark-init cmake-font-lock coffee-mode
-                    company-auctex company-box company-prescient
-                    company-quickhelp company-reftex
-                    counsel-projectile css-eldoc dap-mode diffview
-                    diminish exec-path-from-shell flycheck
-                    goto-char-preview goto-chg goto-last-point
-                    goto-line-preview gruber-darker-theme haml-mode
-                    hl-todo ivy-prescient ivy-yasnippet js2-refactor
-                    json-mode lsp-ivy lsp-ui magic-latex-buffer magit
-                    markdown-toc mocha modern-cpp-font-lock nyan-mode
-                    php-mode prettier-js pyvenv rainbow-mode
-                    restclient scss-mode treemacs-projectile
-                    typescript-mode web-mode wgrep yasnippet-snippets)))
+   '(go-mode auctex-latexmk benchmark-init cmake-font-lock coffee-mode company-auctex company-box company-prescient company-quickhelp company-reftex counsel-projectile css-eldoc dap-mode diffview diminish exec-path-from-shell flycheck goto-char-preview goto-chg goto-last-point goto-line-preview gruber-darker-theme haml-mode hl-todo ivy-prescient ivy-yasnippet js2-refactor json-mode lsp-ivy lsp-ui magic-latex-buffer magit markdown-toc mocha modern-cpp-font-lock nyan-mode php-mode prettier-js pyvenv rainbow-mode restclient scss-mode treemacs-projectile typescript-mode web-mode wgrep yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(lsp-ui-sideline-code-action ((t (:inherit warning))))
- '(whitespace-empty ((t (:extend t :background "gray11" :foreground "#ffdd33")))))
-
+ '(whitespace-empty ((t (:extend t :background "gray11" :foreground "#ffdd33"))))
+ '(whitespace-indentation ((t (:extend t :background "#00000000" :foreground "#505050"))))
+ '(whitespace-space ((t (:extend t :background "#00000000" :foreground "#505050"))))
+ '(whitespace-tab ((t (:extend t :background "#00000000" :foreground "#505050")))))
