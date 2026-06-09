b69904ae8a3f00f155d5bfbe391f5dba4b1210c0
neochin <neochin2012@gmail.com>
2026-06-09 18:02:43 +0800

    python local venv support

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -4,6 +4,19 @@
     (exec-path-from-shell-copy-env "PYTHONPATH")))
 
 (use-package pyvenv
-  :hook (after-init . pyvenv-mode))
+  :hook (after-init . pyvenv-mode)
+  :config
+  (defun my/python-activate-project-venv ()
+    "Find .venv upward from the current file and activate it."
+    (when-let ((root (locate-dominating-file default-directory ".venv")))
+      (let ((venv (expand-file-name ".venv" root)))
+        (when (file-directory-p venv)
+          (setq-local pyvenv-activate venv)
+          (unless (and pyvenv-virtual-env
+                       (string= (expand-file-name pyvenv-virtual-env)
+                                (expand-file-name venv)))
+            (pyvenv-activate venv))))))
+  (add-hook 'python-mode-hook #'my/python-activate-project-venv)
+  (add-hook 'python-ts-mode-hook #'my/python-activate-project-venv))
 
 (provide 'init-python)
